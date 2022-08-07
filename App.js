4e1d8a0eae56a3922524adb2bb0e6ef7fc0ad0c3
kusaymoo <plahsoekusay@gmail.com>
2022-08-07 20:38:23 +1200

    Update App.js
    generate random quotes

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { ImageBackground, Dimensions, Platform, StyleSheet, TouchableNativeFeedback, Text, View, SafeAreaView, Image, Button } from 'react-native';
+import { ImageBackground, Dimensions, Platform, StyleSheet, TouchableNativeFeedback, Text, View, SafeAreaView, Image, Button, ListViewBase } from 'react-native';
 import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks';
 
 export default function App() {
@@ -13,6 +13,8 @@ export default function App() {
   const [imageSrc, setImageSrc] = useState(getRandomImage)
   const {height} = Dimensions.get('window')
 
+  const [randQuote, setRandQuote] = useState("Hello World")
+
   const getRandomQuote = async () =>
   {
       //api for quotes
@@ -20,23 +22,20 @@ export default function App() {
 
       // fetch the data from api
       const response = await fetch(url);
-      console.log(typeof response);
+
       //convert response to json and store it in quotes array
       const allQuotes = await response.json();
 
       // Generates a random number between 0 and the length of the quotes array
       const indx = Math.floor(Math.random()*allQuotes.length);
 
-      const quote = allQuotes[indx]['text'];
-      //console.log(quote);
-      //Store the quote present at the randomly generated index
-      //const quote=allQuotes[indx].text;]
-
-      return quote
+      setRandQuote(allQuotes[indx]['text'])
   }
 
-  console.log(String(getRandomQuote()));
-  const [randQuote, setQuote] = useState(String(getRandomQuote()))
+  let changeImage = () => {
+      getRandomQuote();
+      setImageSrc(getRandomImage);
+  }
 
   return (
     <SafeAreaView style={
@@ -63,7 +62,7 @@ export default function App() {
       </View>
       <Button 
         color="orange"
-        title='New Image' onPress={() => setImageSrc(getRandomImage)}
+        title='New Image' onPress={() => changeImage()}
       />
       <View 
         style={
